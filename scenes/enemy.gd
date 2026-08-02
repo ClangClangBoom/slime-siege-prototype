9746e81f5608aa66cd63c0b1d58b1f0f8ec1af5f
ClangClangBoom <ClangClangBoom@gmail.com>
2026-08-02 10:49:15 -0500

    added tilset, updated player sprite to knight, refactored code to prepare for instanced mob spawning, instead of preset entities in the scene

--- scenes/enemy.gd
+++ scenes/enemy.gd
@@ -1,14 +1,16 @@
 extends CharacterBody2D
 
 var speed = 75
+var player: Node2D # or CharacterBody2D / CharacterBody3D
 
-@export var player: Node2D #drag player into this slot in the inspector
+func _ready():
+	#Finds the player automatically when the enmy enters the game
+	player = get_tree().get_first_node_in_group("player")
 
 func _physics_process(delta: float) -> void:
-	if player:
+	if is_instance_valid(player):
 		#calculate direction to player
 		var direction = (player.global_position - global_position).normalized()
-		
 		#if we are not close enough to the player lets keep moving
 		if player.global_position.distance_to(position) > 50:
 			#move the enemy
@@ -17,6 +19,6 @@ func _physics_process(delta: float) -> void:
 		else:	#stop moving and exit the function
 			return
 		
-		#optional flip sprite
+		#flip sprite horizontally
 		if direction.x != 0:
 			$Sprite2D.flip_h = direction.x < 0
--- scenes/player.gd
+++ scenes/player.gd
@@ -13,3 +13,7 @@ func _physics_process(delta: float) -> void:
 		velocity = velocity.move_toward(Vector2.ZERO, SPEED)
 
 	move_and_slide()
+	
+	#optional flip sprite
+	if direction.x != 0:
+		$AnimatedSprite2D.flip_h = direction.x > 0
